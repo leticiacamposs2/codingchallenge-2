8c6a890758c35534a8f8902b2a0f7c90630106c2
LETICIA CAMPOS DA SILVA <leticia.campos@sp01.local>
2019-03-12 11:54:23 -0300

    criando um modulo compartilhado para os servicos

--- project-auction/src/app/app.module.ts
+++ project-auction/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,15 +5,18 @@ import { ThfI18nModule, ThfI18nConfig } from '@totvs/thf-ui/services/thf-i18n';
 import { ThfModule } from '@totvs/thf-ui';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { LiteralService } from './i18n/literal.service';
 import { generalEn } from './i18n/general-en';
 import { generalPt } from './i18n/general-pt';
+
 import { HomeComponent } from './home/home.component';
 import { SketchComponent } from './auctions/sketch/sketch.component';
 import { MyAuctionsComponent } from './auctions/my-auctions/my-auctions.component';
 import { FinishedAuctionsComponent } from './auctions/finished-auctions/finished-auctions.component';
 import { CardComponent } from './auctions/card/card.component';
+import { SharedModule } from './shared/shared.module';
 
 const i18nConfig: ThfI18nConfig = {
   default: {
@@ -43,7 +45,8 @@ const i18nConfig: ThfI18nConfig = {
     BrowserModule,
     AppRoutingModule,
     ThfModule,
-    ThfI18nModule.config(i18nConfig)
+    ThfI18nModule.config(i18nConfig),
+    SharedModule
   ],
   providers: [LiteralService],
   bootstrap: [AppComponent]
