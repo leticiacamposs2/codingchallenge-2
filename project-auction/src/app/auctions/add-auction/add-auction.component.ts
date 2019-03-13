e4ece0427603266bad51c8807341acc5fc755ec3
LETICIA CAMPOS DA SILVA <leticia.campos@sp01.local>
2019-03-13 15:11:50 -0300

    linkando o formGroup criado no ts e atribuindo ao html

--- project-auction/src/app/auctions/add-auction/add-auction.component.ts
+++ project-auction/src/app/auctions/add-auction/add-auction.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ThfSelectOption } from '@totvs/thf-ui';
-
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-add-auction',
   templateUrl: './add-auction.component.html',
@@ -8,6 +8,7 @@ import { ThfSelectOption } from '@totvs/thf-ui';
 })
 export class AddAuctionComponent implements OnInit {
   public bidTypeOptions: Array<ThfSelectOption>;
+  public formAuction: FormGroup;
 
   constructor() { }
 
