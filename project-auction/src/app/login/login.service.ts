f183c25261e3db987edff23a5708ac1917a5526f
LETICIA CAMPOS DA SILVA <leticia.campos@sp01.local>
2019-03-14 10:05:31 -0300

    por limitação do THF e pra seguir a documentacao do desafio, mantem a variavel login e passar o valor dela pra variavel email

--- project-auction/src/app/login/login.service.ts
+++ project-auction/src/app/login/login.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from './../shared/services/http.service';
 import { Injectable } from '@angular/core';
 import { Login } from './login';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class LoginService {
 
   constructor(private _http: HttpService) { }
 
-  loginUser(data: Login) {
+  loginUser(data: Login): Observable<any> {
+    data.email = data.login;
     return this._http.post('auth/new', data);
    }
 
