237ea1b122c4de832c605b2d8000c6f1925e4147
LETICIA CAMPOS DA SILVA <leticia.campos@sp01.local>
2019-03-15 15:37:15 -0300

    configuracao de teste para subir os componentes

--- project-auction/src/app/auctions/sketch/sketch.component.spec.ts
+++ project-auction/src/app/auctions/sketch/sketch.component.spec.ts
@@ -1,16 +1,58 @@
+import { ThfModule } from '@totvs/thf-ui';
+import { LiteralService } from 'src/app/i18n/literal.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SketchComponent } from './sketch.component';
+import { Component } from '@angular/core';
+import { AuctionsService } from '../auctions.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
-describe('SketchComponent', () => {
+function MockComponent(options: Component) {
+  const metadata: Component = {
+    selector: options.selector,
+    template: options.template || '',
+    inputs: options.inputs,
+    outputs: options.outputs
+  };
+
+  class Mock { }
+
+  return Component(metadata)(Mock as any);
+}
+
+  // tslint:disable-next-line:max-classes-per-file
+class MockAuctionsService {
+    getSketchsAuctions() {
+      return of();
+    }
+   }
+
+  // tslint:disable-next-line:max-classes-per-file
+class MockLiteralService {
+  public literalsAuction = {
+    sketch: {
+      titleSketch: 'Rascunhos'
+    },
+  };
+}
+
+fdescribe('SketchComponent', () => {
   let component: SketchComponent;
   let fixture: ComponentFixture<SketchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SketchComponent ]
+      declarations: [SketchComponent,
+                     MockComponent({selector: 'app-card', inputs: ['t-id', 't-name', 't-base_price',
+                                                                   't-photo', 't-bid_type', 't-bid_step'],
+                                                                    outputs: ['t-auction-info']})
+                    ],
+                    imports: [RouterTestingModule.withRoutes([]), ThfModule],
+                    providers: [{provide: AuctionsService, useClass: MockAuctionsService},
+                                {provide: LiteralService, useClass: MockLiteralService}]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
